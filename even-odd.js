c57bd154f3b1ca2787140a9ad8f2e33ff941adde
Idba1 <moniraislamself@gmail.com>
2024-02-08 05:14:58 +0600

    even-odd

---
+++ even-odd.js
@@ -0,0 +1,12 @@
+// const num = 8;
+function checkEvenOrOdd(x){
+    if(x%2 === 0){
+        return "Even";
+    }
+    else{
+        return "odd";
+    }
+}
+
+let result = checkEvenOrOdd(999);
+console.log(result);
